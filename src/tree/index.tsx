e6df2121c1836be94c5f6e602cea51180d7523ec
Atal Neupane <atalneupane1@gmail.com>
2024-05-29 17:52:01 -0500

    Added data along with udpated test snapshop

--- src/tree/index.tsx
+++ src/tree/index.tsx
@@ -1,33 +1,23 @@
 import "./index.css";
 import React from 'react';
+import data from "./data.json";
 
-const Tree = () => {
-  return (
-    <div className="tree">
-      1 root
-      <div className="children">
-        <div className="node">1.1 ant</div>
-        <div className="node">
-          1.2 bear
-          <div className="children">
-            <div className="node">1.2.1 cat</div>
-            <div className="node">
-              1.2.2 dog
-              <div className="children">
-                <div className="node">1.2.2.1 elephant</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="node">
-          1.3 frog
-          <div className="children">
-            <div className="node">1.3.1 dolphin</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+type Node = {
+  name: string;
+  children?: Node[];
 };
+
+const renderTree = (node: Node) => (
+  <div className="node">
+    {node.name}
+    {node.children && (
+      <div className="children">
+        {node.children.map((child) => renderTree(child))}
+      </div>
+    )}
+  </div>
+);
+
+const Tree = () => <div className="tree">{renderTree(data)}</div>;
 
 export default Tree;
